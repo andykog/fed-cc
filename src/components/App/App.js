2d25c3d66bf2f831d4fee52f61881ddaed67579d
Yaniv Efraim <yaniv.efraim@gmail.com>
2018-05-08 18:44:07 +0300

    First test: should start a new game + wix-style-react

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import {translate} from 'react-i18next';
+import Input from 'wix-style-react/Input';
+import Button from 'wix-style-react/Button';
+import Label from 'wix-style-react/Label';
 import s from './App.scss';
 import PropTypes from 'prop-types';
-
 
 class App extends React.Component {
   static propTypes = {
     t: PropTypes.func
   };
 
+  constructor() {
+    super();
+    this.state = {
+      player1: '',
+      player2: ''
+    }
+  }
   render() {
     const {t} = this.props;
     return (
       <div className={s.root}>
-        <div className={s.header}>
-          <h2>{t('app.title')}</h2>
-        </div>
-        <p className={s.intro}>
-          {t('app.intro')}
-        </p>
+        <Input dataHook="p1-input" onChange={evt => this.setState({player1: evt.target.value})}/>
+        <Input dataHook="p2-input" onChange={evt => this.setState({player2: evt.target.value})}/>
+        <Button dataHook="new-game">New Game</Button>
+        <Label dataHook="p1-title">{this.state.player1}</Label>
+        <Label dataHook="p2-title">{this.state.player2}</Label>
       </div>
     );
   }
--- test/e2e/app.e2e.js
+++ test/e2e/app.e2e.js
@@ -1,16 +1,27 @@
 import {expect} from 'chai';
+import {inputTestkitFactory, buttonTestkitFactory, labelTestkitFactory} from 'wix-style-react/dist/testkit/puppeteer';
 import {beforeAndAfter, app} from './../environment';
 import './e2e-common';
 
 describe('React application', () => {
   beforeAndAfter();
 
-  describe('open page', () => {
-    it('should display title', async () => {
-      const page = await browser.newPage();
-      await page.goto(app.getUrl('/'));
-      await page.waitForSelector('h2', {timeout: 1000});
-      expect(await page.$eval('h2', e => e.innerText)).to.equal('Hello World!');
-    });
+  it('should start a new game', async () => {
+    const player1 = 'Yaniv';
+    const player2 = 'Computer';
+    const page = await browser.newPage();
+    await page.goto(app.getUrl('/'));
+    const p1Testkit = await inputTestkitFactory({dataHook: 'p1-input', page});
+    const p2Testkit = await inputTestkitFactory({dataHook: 'p2-input', page});
+    const buttonTestkit = await buttonTestkitFactory({dataHook: 'new-game', page});
+    const p1LabelTestkit = await labelTestkitFactory({dataHook: 'p1-title', page});
+    const p2LabelTestkit = await labelTestkitFactory({dataHook: 'p2-title', page});
+
+    await p1Testkit.enterText(player1);
+    await p2Testkit.enterText(player2);
+    await buttonTestkit.click();
+
+    expect(await p1LabelTestkit.getLabelText()).to.equal(player1);
+    expect(await p2LabelTestkit.getLabelText()).to.equal(player2);
   });
 });
